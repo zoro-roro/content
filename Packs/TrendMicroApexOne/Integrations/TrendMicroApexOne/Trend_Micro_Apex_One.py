9d45a701236e89562084187eba2238662fc0fddb
gberger <gberger@paloaltonetworks.com>
2020-06-07 15:53:39 +0300

    add list agents ans list servers commands

--- Packs/TrendMicroApexOne/Integrations/TrendMicroApexOne/Trend_Micro_Apex_One.py
+++ Packs/TrendMicroApexOne/Integrations/TrendMicroApexOne/Trend_Micro_Apex_One.py
@@ -23,6 +23,7 @@ class Client(BaseClient):
         super().__init__(base_url, app_id, api_key, verify, proxy)
         self._app_id = app_id
         self._api_key = api_key
+        self._headers = {'Authorization': 'Bearer {}', 'Content-Type': 'application/json;charset=utf-8'}
 
     def create_checksum(self, http_method, api_path, headers, request_body):
         """
@@ -38,16 +39,77 @@ class Client(BaseClient):
         base64_string = base64.b64encode(hashlib.sha256(str.encode(string_to_hash)).digest()).decode('utf-8')
         return base64_string
 
-    def create_jwt_token(self, http_method, api_path, headers, request_body, iat=time.time(), algorithm='HS256',
-                           version='V1', ):
-        checksum = self.create_checksum(http_method, api_path, headers, request_body)
+    def create_jwt_token(self, http_method, url, headers='', request_body='', iat=time.time(), algorithm='HS256',
+                         version='V1', ):
+        checksum = self.create_checksum(http_method, url, headers, request_body)
 
         payload = {'appid': self._app_id,
                    'iat': iat,
                    'version': version,
                    'checksum': checksum}
         token = jwt.encode(payload, self._api_key, algorithm=algorithm).decode('utf-8')
+        self._headers['Authorization'] = f'Bearer {token}'
         return token
+
+    def list_security_agents(self, entity_id, ip_address, mac_address, host_name, product, managing_server_id):
+        url = '/WebApp/API/AgentResource/ProductAgents'
+        self.create_jwt_token('GET', self._base_url + url)
+        params = {}
+        if entity_id:
+            params['entity_id'] = entity_id
+        if ip_address:
+            params['ip_address'] = ip_address
+        if mac_address:
+            params['mac_address'] = mac_address
+        if host_name:
+            params['host_name'] = host_name
+        if product:
+            params['product'] = product
+        if managing_server_id:
+            params['managing_server_id'] = managing_server_id
+
+        res = self._http_request('GET', url, headers=self._headers, params=params)
+
+    def list_servers(self, entity_id, ip_address, mac_address, host_name, product, managing_server_id):
+        url = '/API/ServerResource/ProductServers'
+        self.create_jwt_token('GET', self._base_url + url)
+        params = {}
+        if entity_id:
+            params['entity_id'] = entity_id
+        if ip_address:
+            params['ip_address'] = ip_address
+        if mac_address:
+            params['mac_address'] = mac_address
+        if host_name:
+            params['host_name'] = host_name
+        if product:
+            params['product'] = product
+        if managing_server_id:
+            params['managing_server_id'] = managing_server_id
+
+        res = self._http_request('GET', url, headers=self._headers, params=params)
+
+
+def list_servers_command(client, args):
+    entity_id = args.get('entity_id')
+    ip_address = args.get('ip')
+    mac_address = args.get('mac_address')
+    host_name = args.get('host_name')
+    product = args.get('product')
+    managing_server_id = args.get('managing_server_id')
+
+    res = client.list_servers(entity_id, ip_address, mac_address, host_name, product, managing_server_id)
+
+
+def list_security_agents_command(client, args):
+    entity_id = args.get('entity_id')
+    ip_address = args.get('ip')
+    mac_address = args.get('mac_address')
+    host_name = args.get('host_name')
+    product = args.get('product')
+    managing_server_id = args.get('managing_server_id')
+
+    res = client.list_security_agents(entity_id, ip_address, mac_address, host_name, product, managing_server_id)
 
 
 def test_module(client):
@@ -64,40 +126,6 @@ def test_module(client):
     result = client.create_jwt_token('GET', client._base_url, 'application/json', '')
     if isinstance(result, (str,)):
         return 'ok'
-
-
-def say_hello_command(client, args):
-    """
-    Returns Hello {somename}
-
-    Args:
-        client (Client): HelloWorld client.
-        args (dict): all command arguments.
-
-    Returns:
-        Hello {someone}
-
-        readable_output (str): This will be presented in the war room - should be in markdown syntax - human readable
-        outputs (dict): Dictionary/JSON - saved in the incident context in order to be used as inputs
-                        for other tasks in the playbook
-        raw_response (dict): Used for debugging/troubleshooting purposes -
-                            will be shown only if the command executed with raw-response=true
-    """
-    name = args.get('name')
-
-    result = client.say_hello(name)
-
-    # readable output will be in markdown format - https://www.markdownguide.org/basic-syntax/
-    readable_output = f'## {result}'
-    outputs = {
-        'hello': result
-    }
-
-    return (
-        readable_output,
-        outputs,
-        result  # raw response - the original response
-    )
 
 
 def fetch_incidents(client, last_run, first_fetch_time):
@@ -182,8 +210,26 @@ def main():
             demisto.setLastRun(next_run)
             demisto.incidents(incidents)
 
-        elif demisto.command() == 'helloworld-say-hello':
-            return_outputs(*say_hello_command(client, demisto.args()))
+        elif demisto.command() == 'trendmicro-apex-managed-servers-list':
+            return_outputs(*list_servers_command(client, demisto.args()))
+        elif demisto.command() == 'trendmicro-apex-security-agents-list':
+            return_outputs(*list_security_agents_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-logs-list':
+        #     return_outputs(*list_logs_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-udso-add':
+        #     return_outputs(*add_udso_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-udso-file-add':
+        #     return_outputs(*add_udso_file_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-endpoint-sensors-list':
+        #     return_outputs(*list_sensors_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-process-terminate':
+        #     return_outputs(*process_terminate_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-root-cause-investigation-get-by-task-id':
+        #     return_outputs(*get_root_cause_investigation_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-investigation-result-list-by-status':
+        #     return_outputs(*get_investigation_results_list_by_status_command(client, demisto.args()))
+        # elif demisto.command() == 'trendmicro-apex-investigation-result-list':
+        #     return_outputs(*get_investigation_results_list_command(client, demisto.args()))
 
     # Log exceptions
     except Exception as e:
